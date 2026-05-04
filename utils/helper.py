b117e5fb951141bf024be0b090ff96eac37b001e
Karmadipsinh  jadeja <karmadipsinhjadeja7@gmail.com>
2026-05-04 09:46:25 +0530

    Add files via upload

---
+++ utils/helper.py
@@ -0,0 +1,10 @@
+category_map = {
+    "cs.CL": "Natural Language Processing",
+    "cs.CV": "Computer Vision",
+    "cs.AI": "Artificial Intelligence",
+    "cs.LG": "Machine Learning",
+    "cs.NE": "Deep Learning",
+    "cs.CR": "Cyber Security",
+    "cs.NI": "Networking",
+    "cs.SE": "Software Engineering"
+}
